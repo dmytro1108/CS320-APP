3f5d25946d31cdd4f8191d4c2e2dea2d04b0e19a
dima hodarenko <166257926+dmytro1108@users.noreply.github.com>
2026-05-18 13:44:35 -0700

    Revert "theme: cyberpunk styling + typing effect & drop fix + debug"

--- src/main/database/Tests/BoardRepo.test.ts
+++ src/main/database/Tests/BoardRepo.test.ts
@@ -1,4 +1,3 @@
-import { beforeEach, describe, it, expect, test } from 'vitest';
 // BoardRepo Structure Test
 // -    ensure that all values placed inside test Board are accurate
 // -    ensures that after an update() all values were correctly changed in the DB
